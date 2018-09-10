7afdf7a228904d1c23dfdc38fd55797e1ddf3d8d
zhuchentong <2037630@163.com>
2018-09-10 10:28:16 +0800

    add iview weapp components

--- src/pages.js
+++ src/pages.js
@@ -2,7 +2,10 @@ module.exports = [
   {
     path: 'pages/index.vue',
     config: {
-      navigationBarTitleText: '主页'
+      navigationBarTitleText: '主页',
+      usingComponents: {
+        "i-button": "../../static/iview-weapp/button/index"
+      }
     }
   },
   {
